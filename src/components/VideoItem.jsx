2711443b46e11cdff7f14c403b8e815fb6ec4b3e
Navan Sami <navan.sami@yahoo.com>
2020-11-10 15:28:30 +0200

    added styling to the list

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,9 +1,17 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({video}) => {
   return(
-    <div>
-      { video.snippet.thumbnails.default }
+    <div className="video-item item" >
+      <img 
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url} 
+        alt="youtube video cover"
+      />
+      <div className="content" >
+        <div className="header" >{ video.snippet.title }</div>
+      </div>
     </div>
   )
 }
--- src/components/VideosList.jsx
+++ src/components/VideosList.jsx
@@ -6,7 +6,7 @@ const VideosList = ({ videos }) => {
     return <VideoItem video={video} />
   });
 
-  return <div>{renderedList}</div>
+  return <div className="ui relaxed divided list" >{renderedList}</div>
 }
 
 export default VideosList;
