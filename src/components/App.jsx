48a767eec0bbd10af6d8785aac654451c4a6d49a
Navan Sami <navan.sami@yahoo.com>
2020-11-10 15:40:33 +0200

    added callback for selecting a video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideosList from './VideosList';
 
 class App extends React.Component {
-  state = { videos : [] }
+  state = { videos : [], selectedVideo: null }
 
   onTermSubmitted = async term => {
     const response = await youtube.get('/search', {
@@ -13,11 +13,15 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   }
 
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video.snippet});
+  }
+
   render() {
     return(
        <div className="ui container" >
         <SearchBar onTermSubmitted={this.onTermSubmitted} />
-        <VideosList videos={this.state.videos}  />
+        <VideosList onVideoSelect={this.onVideoSelect} videos={this.state.videos}  />
       </div>
     );
   };
